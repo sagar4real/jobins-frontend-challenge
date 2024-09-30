907e3b565f4be5a9dc34d0c1a0b6ca50e5c7940e
sagar4real <smagar816@gmail.com>
2024-09-30 07:16:53 +0545

    app top bar completed

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,17 +1,18 @@
 <script lang="ts" setup>
 import AppSidebar from "./AppSidebar.vue";
+import AppTopbar from "./AppTopbar.vue";
 </script>
 <template>
   <div class="layout-wrapper">
-    <!-- <app-topbar></app-topbar> -->
     <div class="layout-sidebar" style="position: fixed; z-index: 500">
       <app-sidebar></app-sidebar>
     </div>
     <div class="layout-main-container">
       <div class="layout-main">
+        <app-topbar></app-topbar>
+
         <router-view></router-view>
       </div>
-      <!-- <app-footer></app-footer> -->
     </div>
   </div>
 </template>
---
+++ src/layout/AppTopbar.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="layout-topbar flex items-center justify-between py-6 pr-4 ">
+    <div class="topbar-left">
+      <h5 class="text-heading1 font-bold text-[#23272E]">Dashboard</h5>
+    </div>
+    <div class="topbar-right flex items-center gap-6">
+      <div class="notification">
+        <svg
+          width="33"
+          height="38"
+          viewBox="0 0 33 38"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M11.2742 11.4167C11.2742 10.22 12.2442 9.25 13.4408 9.25C14.6375 9.25 15.6075 10.22 15.6075 11.4167C18.1427 12.6154 19.8094 15.1154 19.9408 17.9167V21.1667C20.1062 22.5325 20.9103 23.7387 22.1075 24.4167H4.77417C5.97138 23.7387 6.7755 22.5325 6.94084 21.1667V17.9167C7.07232 15.1154 8.73899 12.6154 11.2742 11.4167"
+            stroke="#4B465C"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M11.2742 11.4167C11.2742 10.22 12.2442 9.25 13.4408 9.25C14.6375 9.25 15.6075 10.22 15.6075 11.4167C18.1427 12.6154 19.8094 15.1154 19.9408 17.9167V21.1667C20.1062 22.5325 20.9103 23.7387 22.1075 24.4167H4.77417C5.97138 23.7387 6.7755 22.5325 6.94084 21.1667V17.9167C7.07232 15.1154 8.73899 12.6154 11.2742 11.4167"
+            stroke="white"
+            stroke-opacity="0.2"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M10.1908 24.4166V25.5C10.1908 27.2949 11.6459 28.75 13.4408 28.75C15.2357 28.75 16.6908 27.2949 16.6908 25.5V24.4166"
+            stroke="#4B465C"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M10.1908 24.4166V25.5C10.1908 27.2949 11.6459 28.75 13.4408 28.75C15.2357 28.75 16.6908 27.2949 16.6908 25.5V24.4166"
+            stroke="white"
+            stroke-opacity="0.2"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <circle cx="23.6707" cy="11.2617" r="9" fill="#EA5455" />
+          <path
+            d="M23.6651 6.601H25.2381V12.425H26.7396V13.7705H25.2381V16H23.7041L23.7106 13.7705H19.3361V12.425L23.6651 6.601ZM20.8181 12.425H23.6976V8.564L20.8181 12.425Z"
+            fill="white"
+          />
+        </svg>
+      </div>
+      <div class="avatar">
+        <svg
+          width="38"
+          height="40"
+          viewBox="0 0 38 40"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g clip-path="url(#clip0_5_2163)">
+            <rect y="0.5" width="38" height="38" fill="#7367F0" />
+            <rect
+              y="0.5"
+              width="38"
+              height="38"
+              fill="white"
+              fill-opacity="0.2"
+            />
+          </g>
+          <circle
+            cx="31.762"
+            cy="33.7397"
+            r="5"
+            fill="#28C76F"
+            stroke="white"
+            stroke-width="2"
+          />
+          <defs>
+            <clipPath id="clip0_5_2163">
+              <rect y="0.5" width="38" height="38" rx="19" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
